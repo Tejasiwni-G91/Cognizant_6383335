30d5c4547d647dd0b9b007367ebf1c889ae21fdf
Tejaswini <goretejaswini91@gmail.com>
2025-06-19 20:46:24 +0530

    Create SingletonPattern.java

---
+++ Practice/Design_Patterns_and_Principles/Codes/SingletonPattern.java
@@ -0,0 +1,40 @@
+package patterns;
+
+
+public class SingletonPattern {
+
+    // Singleton Logger class
+    static class Logger {
+        private static Logger instance;
+
+        private Logger() {
+            System.out.println("Logger instance created");
+        }
+
+        public static Logger getInstance() {
+            if (instance == null) {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void log(String message) {
+            System.out.println("[LOG]: " + message);
+        }
+    }
+
+    // Main method to test the Singleton
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same (Singleton verified)");
+        } else {
+            System.out.println("Different instances (Singleton violated)");
+        }
+    }
+}
